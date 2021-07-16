74cfa1ee1f39a411225a2d8a2164e5cf67462118
muralivetukuri <murali.vetukuri@gmail.com>
2021-07-16 17:09:08 +0530

    variabel and method added in jira class

--- src/test/java/prokarma/ems/Jira.java
+++ src/test/java/prokarma/ems/Jira.java
@@ -2,7 +2,12 @@ package prokarma.ems;
 
 public class Jira {
 
+	String name="murali";
 	
+	public void name() {
+		
+		System.out.println(name);
+	}
 	  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
